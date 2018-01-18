488caaf649da15cb8f36d527720862c0a107c788
peyton <wangpantuo@quancheng-ec.com>
2018-01-18 16:52:21 +0800

    修改发送数据逻辑

--- src/reporter.js
+++ src/reporter.js
@@ -6,19 +6,22 @@ const Cvss = require('cvss');
 const axios = require('axios');
 const nspCfg = require('rc')('nsp', {});
 
-console.log('postDataApi：', nspCfg.postDataApi)
 
-const postData = function (url, params) {
+const postData = function (url, params,callback) {
   if (!url || !params) {
+    callback()
     console.error('==========post-nsp-Data-error==========')
     console.error('url:', url)
     console.error('params:', params)
     console.error('==========post-nsp-Data-error==========')
     return false
   }
-  axios.post(url, params)
+  return axios.post(url, params)
     .then(res => {
       console.log('==========post-nsp-Data-success==========')
+      if (callback){
+        callback()
+      }
     })
     .catch(err => {
       console.error('==========post-nsp-Data-error==========')
@@ -29,44 +32,47 @@ const postData = function (url, params) {
 
 
 exports.error = function (err, args) {
-  postData(nspCfg.postDataApi, { err, args })
-  console.error(Chalk.yellow('(+)'), err.message);
+  return postData(nspCfg.postDataApi, { err, args },function() {
+            console.error(Chalk.yellow('(+)'), err.message);
+          })
 };
 
 exports.success = function (result, args) {
-  postData(nspCfg.postDataApi, { result, args })
+  return postData(nspCfg.postDataApi, { result, args }, function () {
+    console.log(Chalk.green('(+)'), result.message);
+  })
 
-  console.log(Chalk.green('(+)'), result.message);
 };
 
 exports.check = {};
 exports.check.success = function (result, args) {
 
-  postData(nspCfg.postDataApi, { result, args })
+  return postData(nspCfg.postDataApi, { result, args }, function () {
+    if (!result.data.length) {
+      return console.log(Chalk.green('(+)'), result.message);
+    }
 
-  if (!result.data.length) {
-    return console.log(Chalk.green('(+)'), result.message);
-  }
+    console.log(Chalk.red('(+)'), result.message);
 
-  console.log(Chalk.red('(+)'), result.message);
+    result.data.forEach((finding) => {
 
-  result.data.forEach((finding) => {
+      const table = new Table({
+        head: ['', finding.title],
+        colWidths: [12, 68],
+        wordWrap: true
+      });
 
-    const table = new Table({
-      head: ['', finding.title],
-      colWidths: [12, 68],
-      wordWrap: true
+      table.push(['Name', finding.module]);
+      table.push(['CVSS', `${finding.cvss_score} (${Cvss.getRating(finding.cvss_score)})`]);
+      table.push(['Installed', finding.version]);
+      table.push(['Vulnerable', finding.vulnerable_versions === '<=99.999.99999' ? 'All' : finding.vulnerable_versions]);
+      table.push(['Patched', finding.patched_versions === '<0.0.0' ? 'None' : finding.patched_versions]);
+      table.push(['Path', finding.path.join(' > ')]);
+      table.push(['More Info', finding.advisory]);
+
+      console.log(table.toString());
+      console.log();
     });
+  })
 
-    table.push(['Name', finding.module]);
-    table.push(['CVSS', `${finding.cvss_score} (${Cvss.getRating(finding.cvss_score)})`]);
-    table.push(['Installed', finding.version]);
-    table.push(['Vulnerable', finding.vulnerable_versions === '<=99.999.99999' ? 'All' : finding.vulnerable_versions]);
-    table.push(['Patched', finding.patched_versions === '<0.0.0' ? 'None' : finding.patched_versions]);
-    table.push(['Path', finding.path.join(' > ')]);
-    table.push(['More Info', finding.advisory]);
-
-    console.log(table.toString());
-    console.log();
-  });
 };
